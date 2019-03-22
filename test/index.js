7561c63895a819350698e78e1d8162ec6d6dbf1b
richardpenner <richardpenner@me.com>
2019-03-22 13:23:36 -0400

    updated to new URL & adapted to new content format

--- test/index.js
+++ test/index.js
@@ -1,29 +1,29 @@
 'use strict';
 
-
-const Code = require('code');
-const Lab = require('lab');
 const Probables = require('../');
-
-
-const lab = exports.lab = Lab.script();
-const describe = lab.describe;
-const it = lab.it;
-const expect = Code.expect;
-
+const { expect } = require('code');
+const { it, describe } = exports.lab = require('lab').script();
 
 describe('MLB Probable Pitchers', () => {
-
-    it('Gets probables', (done) => {
-
-        const day = '2011/07/23';
-        Probables.get(day, (err, matchups) => {
-
-            expect(err).to.not.exist();
-            expect(matchups).to.exist();
-            expect(matchups.length).to.equal(14);
-
-            done();
+    const day = '2011/07/23';
+    it(`Gets probables on ${day}`, (done) => {
+        Probables.get(day, (err, dailyMatchups) => {
+            try {
+                expect(err).to.not.exist();
+                expect(dailyMatchups).to.exist();
+                expect(dailyMatchups.length).to.equal(15);
+                expect(dailyMatchups[0].teams).to.exist();
+                expect(dailyMatchups[0].teams.away).to.equal("Astros");
+                expect(dailyMatchups[0].pitchers).to.exist();
+                expect(dailyMatchups[0].pitchers.away).to.exist();
+                expect(dailyMatchups[0].pitchers.away.id).to.equal("434643");
+                expect(dailyMatchups[0].pitchers.home).to.exist();
+                expect(dailyMatchups[0].pitchers.home.id).to.equal("448694");
+                done();
+            }
+            catch (err) {
+                return done(err);
+            }
         });
     });
 });
